2548ab3878dffaf4d847c0188745f6a1d3935708
gilvan <gilvananjos10ns@gmail.com>
2025-08-30 21:12:32 -0300

    projeto base

---
+++ divulgacao/divulgacao/src/test/java/projeto/divulgacao/DivulgacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.divulgacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivulgacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
